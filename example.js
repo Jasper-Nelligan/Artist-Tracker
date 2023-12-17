0ec8ed9156ab70483aac3784a95b0678dd6314bf
Jasper Nelligan <jnelligan@protonmail.com>
2023-12-17 13:05:43 -0800

    Get latest releases from last checked date

--- example.js
+++ example.js
@@ -1,40 +1,30 @@
 require('dotenv').config();
 var http = require('http');
+const { stringify } = require('querystring');
 var SpotifyWebApi = require('spotify-web-api-node');
 
-var artistID;
+const spotifyApi = new SpotifyWebApi({
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+});
 
 http.createServer(async function (req, res) {
-    const spotifyApi = new SpotifyWebApi({
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    });
-
     // Save the access token so that it's used in future calls
-    spotifyApi.setAccessToken(await getAccessToken(spotifyApi));
+    spotifyApi.setAccessToken(await getAccessToken());
 
-    // Search artists whose name contains 'Love'
-    spotifyApi.searchArtists('AJR').then(
-        function(data) {
-            artistID = data.body.artists.items[0].id;
-    },  function(err) {
-            console.error(err);
-    });
+    const artistId = await getArtistId('AJR');
 
-    // Get artist's albums
-    spotifyApi.getArtistAlbums(artistID).then(
-        function(data) {
-            console.log('Artist albums: ', data.body);
-        },
-        function(err) {
-            console.error(err);
-    })
+    const latestReleases = await getLatestReleases('2022-07-06', artistId);
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringify(latestReleases))
+    res.end();
 }).listen(8080);
 
-async function getAccessToken(spotifyApi) {
+/**
+ * Returns a promise containing access token
+ */
+async function getAccessToken() {
     return spotifyApi.clientCredentialsGrant().then(
         function(data) {
             return data.body['access_token'];
@@ -44,3 +34,52 @@ async function getAccessToken(spotifyApi) {
         }
     );
 }
+
+/**
+ * Returns a promise containing the artist's id
+ * @param {String} artist 
+ */
+async function getArtistId(artist) {
+    return spotifyApi.searchArtists(artist).then(
+        function(data) {
+            return(data.body.artists.items[0].id);
+    },  function(err) {
+            console.error(err);
+    });
+}
+
+// '2022-07-29'
+async function getLatestReleases(lastDateChecked, artistId) {
+    // const currentDate = getCurrentDate();
+
+    // Get artist's releases
+    const releases = await spotifyApi.getArtistAlbums(artistId).then(
+        async function(data) {
+            return data.body.items;
+        },
+        function(err) {
+            console.error(err);
+        }
+    )
+
+    const newReleases = [];
+
+    // TODO what happens when release date precision is less than a day?
+    releases.forEach(release => {
+        if (release.release_date > lastDateChecked) {
+            newReleases.push(release)
+        }
+    });
+
+    return newReleases;
+}
+
+function getCurrentDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    return(yyyy + '-' + mm + '-' + dd);
+}
+
