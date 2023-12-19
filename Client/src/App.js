4ed2e5532e7a5d229625c343e435b6e14dd58cd9
Jasper Nelligan <jnelligan@protonmail.com>
2023-12-19 14:57:32 -0800

    Add search bar

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <h1>New Releases</h1>
+    <div className="app-container">
+      <div className="new-releases-title">
+        <h1>New Releases</h1>
+      </div>
+      <div className="account">
+        <p>example@gmail.com</p>
+      </div>
+      <div className="search-box">
+        <div className="search-bar">
+          <FontAwesomeIcon icon={faSearch} className='search-icon'/>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="subscriptions-box">
+        <p>World</p>
+      </div>
     </div>
   );
 }
