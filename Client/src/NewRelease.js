a0bb48c896bc3ae28733123072d1849e0ab08c66
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2023-12-29 15:21:28 -0500

    Add linear gradient to new release container

--- Client/src/NewRelease.js
+++ Client/src/NewRelease.js
@@ -1,10 +1,17 @@
 import './NewRelease.css';
-import albumCover from './images/OK_Orchestra Album Cover.png'
+import { useState } from "react";
+import albumCover from './images/OK_Orchestra Album Cover.png';
+import { ColorExtractor } from 'react-color-extractor';
 
 function NewRelease() {
+    const [imgColor, setImgColor] = useState();
+
+    console.log(imgColor);
     return (
         <div className="new-release-container">
-            <img className="new-release-img" src={albumCover} alt="TODO"/>
+            <ColorExtractor getColors={colors => setImgColor(colors)}>
+                <img className="new-release-img" src={albumCover} alt="TODO"/>
+            </ColorExtractor>
             <p>Hello World</p>
         </div>
     )
