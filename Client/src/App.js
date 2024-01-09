a2d6ab1ca969379a043ccd1ef68f624633e94ad1
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-01-09 14:28:21 -0800

    Fix search and subscription box UI

--- Client/src/App.js
+++ Client/src/App.js
@@ -61,25 +61,27 @@ function App() {
       <div className="account">
         <p>example@gmail.com</p>
       </div>
-      <div className="search-box">
-        <div className="search-bar">
-          <FontAwesomeIcon icon={faSearch} className='search-icon'/>
-          <input type="text" placeholder="Search for an artist"/>
+      <div className="side-bar">
+        <div className="search-box">
+          <div className="search-bar">
+            <FontAwesomeIcon icon={faSearch} className='search-icon'/>
+            <input type="text" placeholder="Search for an artist"/>
+          </div>
+          <div className="search-results">
+            <Artist showSubscribeBtn={true}/>
+            <Artist showSubscribeBtn={true}/>
+            <Artist showSubscribeBtn={true}/>
+            <Artist showSubscribeBtn={true}/>
+          </div>
         </div>
-        <div className="search-results">
-          <Artist showSubscribeBtn={true}/>
-          <Artist showSubscribeBtn={true}/>
-          <Artist showSubscribeBtn={true}/>
-          <Artist showSubscribeBtn={true}/>
-        </div>
-      </div>
-      <div className="subscriptions-box">
-        <h3 className="subscriptions-title">Subscriptions</h3>
-        <div className="search-results">
-          <Artist showSubscribeBtn={false}/>
-          <Artist showSubscribeBtn={false}/>
-          <Artist showSubscribeBtn={false}/>
-          <Artist showSubscribeBtn={false}/>
+        <div className="subscriptions-box">
+          <h3 className="subscriptions-title">Subscriptions</h3>
+          <div className="search-results">
+            <Artist showSubscribeBtn={false}/>
+            <Artist showSubscribeBtn={false}/>
+            <Artist showSubscribeBtn={false}/>
+            <Artist showSubscribeBtn={false}/>
+          </div>
         </div>
       </div>
       {renderLinksModal()}
