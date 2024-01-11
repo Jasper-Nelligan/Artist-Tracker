6fd6d6182438aecc68580382ce3136e8e268eebb
Jasper Nelligan <57274383+Jasper-Nelligan@users.noreply.github.com>
2024-01-10 22:54:01 -0800

    Rename variable

--- Client/src/NewRelease.js
+++ Client/src/NewRelease.js
@@ -1,17 +1,16 @@
 import './NewRelease.css';
 import { useState } from "react";
-import albumCover from './images/OK_Orchestra Album Cover.png';
 import { ColorExtractor } from 'react-color-extractor';
 
 function NewRelease({name, type, artist, albumCoverURL, onGetLinks}) {
-    const [imgColor, setImgColor] = useState([]);
+    const [imgColors, setImgColors] = useState([]);
     const containerStyle = {
-        background: `linear-gradient(${imgColor[0]}, ${imgColor[1]}, ${imgColor[2]}, ${imgColor[3]}, ${imgColor[5]})`
+        background: `linear-gradient(${imgColors[0]}, ${imgColors[1]}, ${imgColors[2]}, ${imgColors[3]}, ${imgColors[5]})`
     }
 
     return (
 			<div className="new-release-container" style={containerStyle}>
-					<ColorExtractor getColors={colors => setImgColor(colors)}>
+					<ColorExtractor getColors={colors => setImgColors(colors)}>
 							<img className="new-release-img" src={albumCoverURL} alt="TODO"/>
 					</ColorExtractor>
 					<p>{name}</p>
